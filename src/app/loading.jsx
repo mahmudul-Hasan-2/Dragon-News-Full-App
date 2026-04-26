1785736c117207c18dde624ca91c4fe5292b35d7
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-04-26 20:20:51 +0600

    Deployment Purpose

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,6 +4,7 @@ const loading = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <span className="loading loading-spinner loading-xl"></span>
+      
     </div>
   );
 };
