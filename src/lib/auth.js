56f8522ed5dae15e1a882e12169eaa39786b3901
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-04-26 20:25:13 +0600

    Deployment Purpose

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,12 +6,11 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("Dragon-News");
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL,
-
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
   }),
+  baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID,
